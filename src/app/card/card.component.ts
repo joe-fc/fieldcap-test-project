0f7aa7245a597eb10cdb36a2e63fdd52d99b406d
Joe <54520973+joe-fc@users.noreply.github.com>
2019-08-26 11:04:29 -0600

    Add support for auto-refreshing exchange rates

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -16,9 +16,20 @@ export class CardComponent implements OnInit {
   constructor(private alphaVantageService: AlphaVantageService) { }
 
   ngOnInit() {
-    this.alphaVantageService.get(this.currency).subscribe(result => {
-      this.rate = result['Realtime Currency Exchange Rate']['5. Exchange Rate'];
-    });
+    // save a copy of the current context for use in the setInterval function below
+	let that = this;
+	// update the exchange rate for this currency immidiately
+	that.updateExchangeRate(that.currency);
+	// set a timer to update the exchange rate for this currency that fires every 1 minute 
+    setInterval(() => {
+		that.updateExchangeRate(that.currency);
+    }, 60000);
   }
-
+  
+  updateExchangeRate(sCurrency) {
+     this.alphaVantageService.get(this.currency).subscribe(result => {
+       this.rate = result['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+     });
+  }
+  
 }
